4fccd547ca0d1c3766b3e98962f5f72501082580
Ayman <you@example.com>
2022-07-19 15:56:16 +0200

    /

---
+++ category_in_stock_quantt/models/models.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    categ_id = fields.Many2one(comodel_name="product.category", string="Category", required=False,related='product_id.categ_id',store=True )
+
+
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    categ_id = fields.Many2one(comodel_name="product.category", string="Category", required=False,related='product_id.categ_id',store=True )
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    categ_id = fields.Many2one(comodel_name="product.category", string="Category", required=False,related='product_id.categ_id',store=True )
+
+
+#
+# class StockValuationLayer(models.Model):
+#     _inherit = 'stock.valuation.layer'
+#
+#     categ_id = fields.Many2one(comodel_name="product.category", string="Category", required=False,related='product_id.categ_id',store=True )
+#
+#
+# #
+# class ReportStockQuantity(models.Model):
+#     _inherit = 'report.stock.quantity'
+#
+#     categ_id = fields.Many2one(comodel_name="product.category", string="Category", required=False,related='product_id.categ_id',store=True )
+#
