4cc9aee338ac463cc42085a9e6d948e34de8d5d4
Odoo Amawy <odooamawy@gmail.com>
2023-12-31 21:10:44 +0200

    edit edit_current_stock

--- edit_current_stock/__manifest__.py
+++ edit_current_stock/__manifest__.py
@@ -9,7 +9,7 @@
     'description': """
         Long description of module's purpose
     """,
-
+    'license': 'OPL-1',
     'author': "My Company",
     'website': "http://www.yourcompany.com",
 
@@ -17,17 +17,17 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '16',
-    'license': 'OPL-1',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock','stock_account'],
+    'depends': ['base', 'stock', 'stock_account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/quants_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
